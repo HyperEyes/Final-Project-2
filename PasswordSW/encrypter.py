d6bef8bc2015a1cd8c038150211d09a620a9f3f0
Qasim Khan <97201966+HyperEyes@users.noreply.github.com>
2022-05-09 22:38:09 -0400

    Update encrypter.py

--- PasswordSW/encrypter.py
+++ PasswordSW/encrypter.py
@@ -1,3 +1,7 @@
+# The job of this program is to use the strong password you have made from the first program to encrypt it.
+# After running the program, you notice two lines, the first line is a long string of random numbers and letters--that is your encrypted pass, the next line is 
+# your password decrypted.
+
 from cryptography.fernet import Fernet
 
 keygen = Fernet.generate_key()
